e0135da90406c276488a61f5acb740e114310403
Kohei Yoshino <kohei.yoshino@gmail.com>
2024-03-27 17:57:12 -0400

    Add support for GitLab

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sveltia CMS Authenticator
 
-This simple [Cloudflare Workers](https://workers.cloudflare.com/) script allows [Sveltia CMS](https://github.com/sveltia/sveltia-cms) (or Netlify/Decap CMS) to [authenticate with GitHub](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps).
+This simple [Cloudflare Workers](https://workers.cloudflare.com/) script allows [Sveltia CMS](https://github.com/sveltia/sveltia-cms) (or Netlify/Decap CMS) to authenticate with [GitHub](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps) or [GitLab](https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow).
 
 You don’t have to use it if you previously had Netlify/Decap CMS and your site is still being deployed to Netlify or if you have already used [another 3rd party OAuth client](https://decapcms.org/docs/external-oauth-clients/).
 
@@ -18,7 +18,9 @@ Alternatively, you can clone the project and run [`wrangler deploy`](https://dev
 
 Once deployed, open your Cloudflare Workers dashboard, select the `sveltia-cms-auth` service, then the worker URL (`https://sveltia-cms-auth.<SUBDOMAIN>.workers.dev`) will be displayed. Copy it for Step 2. It will also be used in Step 4.
 
-### Step 2. Register the Worker as a GitHub OAuth app
+### Step 2. Register the Worker as an OAuth app
+
+#### GitHub
 
 [Register a new OAuth application](https://github.com/settings/applications/new) on GitHub ([details](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/creating-an-oauth-app)) with the following properties, including your Worker URL from Step 1:
 
@@ -29,14 +31,36 @@ Once deployed, open your Cloudflare Workers dashboard, select the `sveltia-cms-a
 
 Once registered, click on the **Generate a new client secret** button. The app’s **Client ID** and **Client Secret** will be displayed. We’ll use them in Step 3 below.
 
+#### GitLab
+
+[Register a new OAuth application](https://gitlab.com/-/user_settings/applications) on GitLab ([details](https://docs.gitlab.com/ee/integration/oauth_provider.html#create-a-user-owned-application)) with the following properties, including your Worker URL from Step 1:
+
+- Name: `Sveltia CMS Authenticator` (or whatever)
+- Redirect URI: `<YOUR_WORKER_URL>/callback`
+- Confidential: Yes
+- Scopes: `api` only
+
+Once registered, the app’s **Application ID** and **Secret** will be displayed. We’ll use them in Step 3 below.
+
 ### Step 3. Configure the Worker
 
 Go back to the `sveltia-cms-auth` service page on Cloudflare, select **Settings** > **Variables**, and add the following Environment Variables to your worker ([details](https://developers.cloudflare.com/workers/platform/environment-variables/#environment-variables-via-the-dashboard)):
 
-- `GITHUB_CLIENT_ID`: Client ID from Step 2
-- `GITHUB_CLIENT_SECRET`: Client Secret from Step 2; click the **Encrypt** button to hide it
+#### GitHub
+
+- `GITHUB_CLIENT_ID`: **Client ID** from Step 2
+- `GITHUB_CLIENT_SECRET`: **Client Secret** from Step 2; click the **Encrypt** button to hide it
 - `GITHUB_HOSTNAME`: Required only if you’re using GitHub Enterprise Server. Default: `github.com`
-- `ALLOWED_DOMAINS`: Your site’s hostname, e.g. `www.example.com`
+
+#### GitLab
+
+- `GITLAB_CLIENT_ID`: **Application ID** from Step 2
+- `GITLAB_CLIENT_SECRET`: **Secret** from Step 2; click the **Encrypt** button to hide it
+- `GITLAB_HOSTNAME`: Required only if you’re using a custom domain. Default: `gitlab.com`
+
+#### Both GitHub and GitLab
+
+- `ALLOWED_DOMAINS`: (Optional) Your site’s hostname, e.g. `www.example.com`
   - Multiple hostnames can be defined as a comma-separated list, e.g. `www.example.com, www.example.org`
   - A wildcard (`*`) can be used to match any subdomain, e.g. `*.example.com` that will match `www.example.com`, `blog.example.com`, `docs.api.example.com`, etc. (but not `example.com`)
   - To match a `www`-less naked domain and all the subdomains, use `example.com, *.example.com`
@@ -49,7 +73,7 @@ Open `admin/config.yml` locally or remotely, and add your Worker URL from Step 1
 
 ```diff
  backend:
-   name: github
+   name: github # or gitlab
    repo: username/repo
    branch: main
 +  base_url: <YOUR_WORKER_URL>
--- src/index.js
+++ src/index.js
@@ -14,10 +14,17 @@ const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
  */
 const handleAuth = async (request, env) => {
   const { url } = request;
-  const { searchParams } = new URL(url);
+  const { origin, searchParams } = new URL(url);
   const provider = searchParams.get('provider');
   const domain = searchParams.get('site_id');
-  const { ALLOWED_DOMAINS, GITHUB_CLIENT_ID, GITHUB_HOSTNAME = 'github.com' } = env;
+
+  const {
+    ALLOWED_DOMAINS,
+    GITHUB_CLIENT_ID,
+    GITHUB_HOSTNAME = 'github.com',
+    GITLAB_CLIENT_ID,
+    GITLAB_HOSTNAME = 'gitlab.com',
+  } = env;
 
   // Check if the domain is whitelisted
   if (
@@ -32,8 +39,7 @@ const handleAuth = async (request, env) => {
 
   // Generate a random string for CSRF protection
   const csrfToken = globalThis.crypto.randomUUID().replaceAll('-', '');
-  // Cookie expires in 10 minutes; Use `SameSite=Lax` to make sure the cookie is sent after redirect
-  const cookie = `csrf-token=${csrfToken}; HttpOnly; Max-Age=600; SameSite=Lax; Secure`;
+  let authURL;
 
   // GitHub
   if (provider === 'github' && GITHUB_CLIENT_ID) {
@@ -43,11 +49,30 @@ const handleAuth = async (request, env) => {
       state: csrfToken,
     });
 
+    authURL = `https://${GITHUB_HOSTNAME}/login/oauth/authorize?${params.toString()}`;
+  }
+
+  // GitLab
+  if (provider === 'gitlab' && GITLAB_CLIENT_ID) {
+    const params = new URLSearchParams({
+      client_id: GITLAB_CLIENT_ID,
+      redirect_uri: `${origin}/callback`,
+      response_type: 'code',
+      scope: 'api',
+      state: csrfToken,
+    });
+
+    authURL = `https://${GITLAB_HOSTNAME}/oauth/authorize?${params.toString()}`;
+  }
+
+  if (authURL) {
     return new Response('', {
       status: 302,
       headers: {
-        'Set-Cookie': cookie,
-        Location: `https://${GITHUB_HOSTNAME}/login/oauth/authorize?${params.toString()}`,
+        Location: authURL,
+        // Cookie expires in 10 minutes; Use `SameSite=Lax` to make sure the cookie is sent by the
+        // browser after redirect
+        'Set-Cookie': `csrf-token=${csrfToken}; HttpOnly; Max-Age=600; SameSite=Lax; Secure`,
       },
     });
   }
@@ -63,7 +88,7 @@ const handleAuth = async (request, env) => {
  */
 const handleCallback = async (request, env) => {
   const { url, headers } = request;
-  const { searchParams } = new URL(url);
+  const { origin, searchParams } = new URL(url);
   const code = searchParams.get('code');
   const csrfToken = searchParams.get('state');
   const csrfTokenCookie = headers.get('Cookie');
@@ -72,36 +97,72 @@ const handleCallback = async (request, env) => {
     return new Response('', { status: 403 });
   }
 
-  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_HOSTNAME = 'github.com' } = env;
+  const {
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    GITHUB_HOSTNAME = 'github.com',
+    GITLAB_CLIENT_ID,
+    GITLAB_CLIENT_SECRET,
+    GITLAB_HOSTNAME = 'gitlab.com',
+  } = env;
+
   let provider;
-  let token;
-  let error;
+  let tokenURL;
+  let requestBody;
 
   // GitHub
   if (GITHUB_CLIENT_ID && GITHUB_CLIENT_SECRET) {
     provider = 'github';
-
-    try {
-      const response = await fetch(`https://${GITHUB_HOSTNAME}/login/oauth/access_token`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          client_id: GITHUB_CLIENT_ID,
-          client_secret: GITHUB_CLIENT_SECRET,
-          code,
-        }),
-      });
-
-      ({ access_token: token, error } = await response.json());
-    } catch ({ message }) {
-      error = message;
-    }
+    tokenURL = `https://${GITHUB_HOSTNAME}/login/oauth/access_token`;
+    requestBody = {
+      code,
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+    };
   }
 
-  if (!provider || !(token || error)) {
+  // GitLab
+  if (GITLAB_CLIENT_ID && GITLAB_CLIENT_SECRET) {
+    provider = 'gitlab';
+    tokenURL = `https://${GITLAB_HOSTNAME}/oauth/token`;
+    requestBody = {
+      code,
+      client_id: GITLAB_CLIENT_ID,
+      client_secret: GITLAB_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: `${origin}/callback`,
+    };
+  }
+
+  if (!provider) {
+    return new Response('', { status: 403 });
+  }
+
+  let token;
+  let error;
+
+  try {
+    const response = await fetch(tokenURL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const { ok, status } = response;
+
+    if (!ok) {
+      throw new Error(`Server responded with status ${status}`);
+    }
+
+    ({ access_token: token, error } = await response.json());
+  } catch ({ message }) {
+    error = message;
+  }
+
+  if (!(token || error)) {
     return new Response('', { status: 403 });
   }
 
@@ -139,6 +200,7 @@ export default {
    * @returns {Promise<Response>} HTTP response.
    * @see https://developers.cloudflare.com/workers/runtime-apis/fetch/
    * @see https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+   * @see https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow
    */
   async fetch(request, env) {
     const { method, url } = request;
